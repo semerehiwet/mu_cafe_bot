77cc6340d20e72c1e7b0913fab59be67810f7755
semerehiwet <semerehiwet5@gmail.com>
2026-03-31 10:30:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = "PUT_YOUR_TOKEN_HERE"
+TOKEN = "8146345458:AAFNkn0CwekS4aluEkYIzO8M5pni6tIAPJE"
 
 main_menu = [["🍽 ምግብ", "📍 Location"], ["❓ Help"]]
 
